b68ee3ec074a2c6001f7270417d347bf5b117159
DragoNikaa <julia.jurczak15@gmail.com>
2026-03-07 19:00:55 +0100

    feat: add is lab 1 tasks

---
+++ semestr_6/integracja_systemow/1_transfer_danych_z_wykorzystaniem_plikow_format_xml/XmlReadWithDomApproach.cs
@@ -0,0 +1,124 @@
+﻿using System.Xml;
+
+namespace _1_transfer_danych_z_wykorzystaniem_plikow_format_xml
+{
+    internal class XmlReadWithDomApproach
+    {
+        internal static void Read(string filePath)
+        {
+            var document = LoadDocument(filePath);
+            var namespaceManager = CreateNamespaceManager(document);
+            var drugs = document.GetElementsByTagName("produktLeczniczy");
+
+            DisplayCount(drugs, "Mometasoni furoas", "Krem");
+            DisplayMultiFormCount(drugs);
+            DisplayTopManufacturers(drugs, "Krem");
+            DisplayTopManufacturers(drugs, "Tabletki");
+            DisplayDrugsWithActiveIngredientCount(namespaceManager, drugs, 5);
+        }
+
+        private static void DisplayCount(XmlNodeList drugs, string activeIngredient, string form)
+        {
+            var count = 0;
+
+            foreach (XmlNode drug in drugs)
+            {
+                if (GetAttribute(drug, "nazwaPowszechnieStosowana") == activeIngredient
+                    && GetAttribute(drug, "nazwaPostaciFarmaceutycznej") == form)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Liczba produktów leczniczych w postaci \"{form}\", " +
+                $"których jedyną substancją czynną jest \"{activeIngredient}\": {count}");
+        }
+
+        private static void DisplayMultiFormCount(XmlNodeList drugs)
+        {
+            var activeIngredientToForms = new Dictionary<string, HashSet<string>>();
+
+            foreach (XmlNode drug in drugs)
+            {
+                var activeIngredient = GetAttribute(drug, "nazwaPowszechnieStosowana");
+                var form = GetAttribute(drug, "nazwaPostaciFarmaceutycznej");
+
+                activeIngredientToForms.TryAdd(activeIngredient, []);
+                activeIngredientToForms[activeIngredient].Add(form);
+            }
+
+            var count = 0;
+
+            foreach (var forms in activeIngredientToForms.Values)
+            {
+                if (forms.Count > 1) count++;
+            }
+
+            Console.WriteLine($"Liczba produktów leczniczych o takiej samej nazwie powszechnej, " +
+                $"pod różnymi postaciami: {count}");
+        }
+
+        private static void DisplayTopManufacturers(XmlNodeList drugs, string form, int maxResults = 3)
+        {
+            var manufacturerToFormCount = new Dictionary<string, int>();
+
+            foreach (XmlNode drug in drugs)
+            {
+                var manufacturer = GetAttribute(drug, "podmiotOdpowiedzialny");
+
+                if (GetAttribute(drug, "nazwaPostaciFarmaceutycznej") == form)
+                {
+                    manufacturerToFormCount[manufacturer] = manufacturerToFormCount.GetValueOrDefault(manufacturer) + 1;
+                }
+            }
+
+            var topManufacturers = manufacturerToFormCount
+                .OrderByDescending(item => item.Value)
+                .Take(maxResults);
+
+            Console.WriteLine($"\nPodmioty produkujące najwięcej \"{form}\":");
+
+            foreach (var (index, item) in topManufacturers.Index())
+            {
+                Console.WriteLine($"{index + 1}. {item.Key}: {item.Value} produktów");
+            }
+        }
+
+        private static void DisplayDrugsWithActiveIngredientCount(XmlNamespaceManager namespaceManager, XmlNodeList drugs, int activeIngredientCount, int maxResults = 5)
+        {
+            var results = 0;
+
+            Console.WriteLine($"\nProdukty lecznicze zawierające {activeIngredientCount} substancji czynnych:");
+
+            foreach (XmlNode drug in drugs)
+            {
+                if (drug.SelectNodes("x:substancjeCzynne/x:substancjaCzynna", namespaceManager)?.Count == activeIngredientCount)
+                {
+                    Console.WriteLine($"- {GetAttribute(drug, "nazwaProduktu")}");
+
+                    results++;
+                    if (results >= maxResults) return;
+                }
+            }
+        }
+
+        private static XmlDocument LoadDocument(string filePath)
+        {
+            var document = new XmlDocument();
+            document.Load(filePath);
+            return document;
+        }
+
+        private static XmlNamespaceManager CreateNamespaceManager(XmlDocument document)
+        {
+            var manager = new XmlNamespaceManager(document.NameTable);
+            manager.AddNamespace("x", "http://rejestry.ezdrowie.gov.pl/rpl/eksport-danych-v6.0.0");
+            return manager;
+        }
+
+        private static string GetAttribute(XmlNode drug, string attribute)
+        {
+            return drug.Attributes!.GetNamedItem(attribute)!.Value!;
+        }
+    }
+}
---
+++ semestr_6/integracja_systemow/1_transfer_danych_z_wykorzystaniem_plikow_format_xml/XmlReadWithXsltDom.cs
@@ -0,0 +1,163 @@
+﻿using System.Xml;
+using System.Xml.XPath;
+
+namespace _1_transfer_danych_z_wykorzystaniem_plikow_format_xml
+{
+    internal class XmlReadWithXsltDom
+    {
+        internal static void Read(string filePath)
+        {
+            var navigator = CreateNavigator(filePath);
+            var namespaceManager = CreateNamespaceManager(navigator);
+
+            DisplayCount(navigator, namespaceManager, "Mometasoni furoas", "Krem");
+            DisplayMultiFormCount(navigator, namespaceManager);
+            DisplayTopManufacturers(navigator, namespaceManager, "Krem");
+            DisplayTopManufacturers(navigator, namespaceManager, "Tabletki");
+            DisplayDrugsWithActiveIngredientCount(navigator, namespaceManager, 5);
+
+            var manufacturer = "InPharm Sp. z o.o.";
+            var formStatistics = GetFormStatistics(navigator, namespaceManager, manufacturer);
+            DrawHistogram(formStatistics, $"Top postacie farmaceutyczne produkowane przez {manufacturer}");
+        }
+
+        private static void DisplayCount(XPathNavigator navigator, XmlNamespaceManager namespaceManager, string activeIngredient, string form)
+        {
+            var query = navigator.Compile($"/x:produktyLecznicze/x:produktLeczniczy" +
+                $"[@nazwaPowszechnieStosowana='{activeIngredient}' and @nazwaPostaciFarmaceutycznej='{form}']");
+            query.SetContext(namespaceManager);
+            var count = navigator.Select(query).Count;
+
+            Console.WriteLine($"Liczba produktów leczniczych w postaci \"{form}\", " +
+                $"których jedyną substancją czynną jest \"{activeIngredient}\": {count}");
+        }
+
+        private static void DisplayMultiFormCount(XPathNavigator navigator, XmlNamespaceManager namespaceManager)
+        {
+            var query = navigator.Compile("/x:produktyLecznicze/x:produktLeczniczy");
+            query.SetContext(namespaceManager);
+            var iterator = navigator.Select(query);
+
+            var activeIngredientToForms = new Dictionary<string, HashSet<string>>();
+
+            while (iterator.MoveNext())
+            {
+                var activeIngredient = GetAttribute(iterator, "nazwaPowszechnieStosowana");
+                var form = GetAttribute(iterator, "nazwaPostaciFarmaceutycznej");
+
+                activeIngredientToForms.TryAdd(activeIngredient, []);
+                activeIngredientToForms[activeIngredient].Add(form);
+            }
+
+            var count = 0;
+
+            foreach (var forms in activeIngredientToForms.Values)
+            {
+                if (forms.Count > 1) count++;
+            }
+
+            Console.WriteLine($"Liczba produktów leczniczych o takiej samej nazwie powszechnej, " +
+                $"pod różnymi postaciami: {count}");
+        }
+
+        private static void DisplayTopManufacturers(XPathNavigator navigator, XmlNamespaceManager namespaceManager, string form, int maxResults = 3)
+        {
+            var query = navigator.Compile($"/x:produktyLecznicze/x:produktLeczniczy" +
+                $"[@nazwaPostaciFarmaceutycznej='{form}']");
+            query.SetContext(namespaceManager);
+            var iterator = navigator.Select(query);
+
+            var manufacturerToFormCount = new Dictionary<string, int>();
+
+            while (iterator.MoveNext())
+            {
+                var manufacturer = GetAttribute(iterator, "podmiotOdpowiedzialny");
+                manufacturerToFormCount[manufacturer] = manufacturerToFormCount.GetValueOrDefault(manufacturer) + 1;
+            }
+
+            var topManufacturers = manufacturerToFormCount
+                .OrderByDescending(item => item.Value)
+                .Take(maxResults);
+
+            Console.WriteLine($"\nPodmioty produkujące najwięcej \"{form}\":");
+
+            foreach (var (index, item) in topManufacturers.Index())
+            {
+                Console.WriteLine($"{index + 1}. {item.Key}: {item.Value} produktów");
+            }
+        }
+
+        private static void DisplayDrugsWithActiveIngredientCount(XPathNavigator navigator, XmlNamespaceManager namespaceManager, int activeIngredientCount, int maxResults = 5)
+        {
+            var query = navigator.Compile($"/x:produktyLecznicze/x:produktLeczniczy[" +
+                $"count(x:substancjeCzynne/x:substancjaCzynna) = {activeIngredientCount}]");
+            query.SetContext(namespaceManager);
+            var iterator = navigator.Select(query);
+
+            var results = 0;
+
+            Console.WriteLine($"\nProdukty lecznicze zawierające {activeIngredientCount} substancji czynnych:");
+
+            while (iterator.MoveNext() && results < maxResults)
+            {
+                Console.WriteLine($"- {GetAttribute(iterator, "nazwaProduktu")}");
+                results++;
+            }
+        }
+
+        private static Dictionary<string, int> GetFormStatistics(XPathNavigator navigator, XmlNamespaceManager namespaceManager, string manufacturer)
+        {
+            var query = navigator.Compile($"/x:produktyLecznicze/x:produktLeczniczy" +
+                $"[@podmiotOdpowiedzialny='{manufacturer}']");
+            query.SetContext(namespaceManager);
+            var iterator = navigator.Select(query);
+
+            var statistics = new Dictionary<string, int>();
+
+            while (iterator.MoveNext())
+            {
+                var value = GetAttribute(iterator, "nazwaPostaciFarmaceutycznej")!;
+                statistics[value] = statistics.GetValueOrDefault(value) + 1;
+            }
+            return statistics;
+        }
+
+        private static void DrawHistogram(Dictionary<string, int> statistics, string title, int maxResults = 10)
+        {
+            Console.WriteLine($"\n{title}:");
+
+            var sortedStatistics = statistics
+                .OrderByDescending(item => item.Value)
+                .Take(maxResults);
+            var maxValue = sortedStatistics.First().Value;
+            var maxLabelLength = sortedStatistics.Max(item => item.Key.Length);
+            var maxBarWidth = 50;
+
+            foreach (var (index, item) in sortedStatistics.Index())
+            {
+                var barWidth = (int)((double)item.Value / maxValue * maxBarWidth);
+                var bar = new string('█', barWidth);
+
+                Console.WriteLine($"{index + 1,2}. {item.Key.PadRight(maxLabelLength)} [{item.Value,4}] {bar}");
+            }
+        }
+
+        private static XPathNavigator CreateNavigator(string filePath)
+        {
+            var document = new XPathDocument(filePath);
+            return document.CreateNavigator();
+        }
+
+        private static XmlNamespaceManager CreateNamespaceManager(XPathNavigator navigator)
+        {
+            var manager = new XmlNamespaceManager(navigator.NameTable);
+            manager.AddNamespace("x", "http://rejestry.ezdrowie.gov.pl/rpl/eksport-danych-v6.0.0");
+            return manager;
+        }
+
+        private static string GetAttribute(XPathNodeIterator iterator, string attribute)
+        {
+            return iterator.Current!.GetAttribute(attribute, "");
+        }
+    }
+}
