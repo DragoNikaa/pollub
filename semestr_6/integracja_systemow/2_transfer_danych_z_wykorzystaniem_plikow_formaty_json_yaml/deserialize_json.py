d82cbbc0d89fa01fee530bd70d34f95fa7ad1f50
DragoNikaa <julia.jurczak15@gmail.com>
2026-03-20 18:34:27 +0100

    feat: add is lab 2 tasks

---
+++ semestr_6/integracja_systemow/2_transfer_danych_z_wykorzystaniem_plikow_formaty_json_yaml/deserialize_json.py
@@ -0,0 +1,25 @@
+﻿import json
+from collections import defaultdict
+
+
+class DeserializeJson:
+    def __init__(self, file_path: str):
+        with open(file_path, encoding="utf8") as file:
+            self.data = json.load(file)
+
+    def display_statistics(self) -> None:
+        province_to_department_type_count: dict[str, dict[str, int]] = defaultdict(lambda: defaultdict(int))
+
+        for department in self.data:
+            province = department["Województwo"]
+            department_type = department["typ_JST"]
+
+            province_to_department_type_count[province][department_type] += 1
+
+        print("Liczba urzędów w każdym województwie z podziałem na typy JST:")
+
+        for province, department_type_count in province_to_department_type_count.items():
+            print(f"- {province}:")
+
+            for department_type, count in department_type_count.items():
+                print(f"\t- {department_type}: {count}")
